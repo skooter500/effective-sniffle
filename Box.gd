4a3744c85c19911789f5b9e9e3830e75a18a26d1
Bryan Duggan <skooter500@gmail.com>
2022-11-24 19:11:58 +0000

    my first godot project

---
+++ Box.gd
@@ -0,0 +1,29 @@
+extends Spatial
+
+export var speed = 5.0
+export var rotSpeed = 5.0
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):		
+	if Input.is_action_pressed("ui_up"):    
+		print("up")
+		transform.origin += transform.basis.z * speed * delta;
+	if Input.is_action_pressed("ui_down"):    
+		transform.origin -= transform.basis.z * speed * delta;
+	if Input.is_action_pressed("ui_left"):    
+		rotate(Vector3.UP, rotSpeed * delta)
+	if Input.is_action_pressed("ui_right"):    
+		rotate(Vector3.UP, - rotSpeed * delta)
+	
+	
